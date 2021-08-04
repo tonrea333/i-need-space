40250c34c23b3d5c7e9386e365753f552160a23a
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-08-03 21:06:25 -0400

    all

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 // List of const 
 
 const buttonSearch = document.querySelector("#search");
-const apiEntry = document.querySelector("#api-key");
-const addressEntry = document.querySelector("#address");
+
+
+
 
 buttonSearch.addEventListener("click", searchNorad);
 
 function searchNorad() {
+    const apiEntry = document.querySelector("#api-key");
+    const addressEntry = document.querySelector("#address");
+  
 
-    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/"+addressEntry+".json?access_token="+apiEntry"
+    fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/"+addressEntry+".json?"+apiEntry)
     .then(function(bootdata) {
         return bootdata.json();
     })
     .then (function(data){
 console.log(data, "data")
+console.log(addressEntry, "address");
+console.log(apiEntry,"key")
+
     })
 }
